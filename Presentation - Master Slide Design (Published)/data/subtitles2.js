1559b05cd5dddb7426a635b8d7ff18265748da51
mist-jessica-foley <jefoley@csumb.edu>
2025-12-05 14:50:35 -0800

    Project

---
+++ Presentation - Master Slide Design (Published)/data/subtitles2.js
@@ -0,0 +1,6 @@
+(function () {
+	const subtitles = {
+			data: "WEBVTT\n\n00:00.800 --> 00:04.400\nWelcome to Module 1. Sewing parts and safety. We\'ll get\n\n00:04.900 --> 00:09.040\noriented to the Opal 690 Q and set up safe habits before any stitching.\n\n"
+		};
+	window["st_{B2A6E81C-8B00-4927-922D-A30A8BEC661F}"]?.("sndAsset2", subtitles);
+})();
---
+++ Presentation - Master Slide Design (Published)/data/subtitles5.js
@@ -0,0 +1,6 @@
+(function () {
+	const subtitles = {
+			data: "WEBVTT\n\n00:01.040 --> 00:04.320\nExplore the highlights to learn each part, visit them\n\n00:04.820 --> 00:06.960\nall and then choose next to continue.\n\n"
+		};
+	window["st_{B2A6E81C-8B00-4927-922D-A30A8BEC661F}"]?.("sndAsset5", subtitles);
+})();
